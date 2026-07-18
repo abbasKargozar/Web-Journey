f5990313b2358e34727b8acdb1405bcecc2d4f26
abbasKargozar <abbas.kargozar7@gmail.com>
2026-07-18 23:15:01 +0330

    Add table wiht CSS design

--- 007_Style_for_001/javaScript.js
+++ 007_Style_for_001/javaScript.js
@@ -5,5 +5,5 @@ const styleButton = document.getElementById("style-changer")
 styleButton.addEventListener("click", () => {
     linkHolder.disabled = !linkHolder.disabled;
     styleButton.textContent = linkHolder.disabled
-    ? "Click to enable Style" : "Click to disable Style";
+    ? "Click to enable CSS" : "Click to disable CSS";
 });
